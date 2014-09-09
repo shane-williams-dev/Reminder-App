b9ccf3d3aad59ed951508946056d842389589d02
shane-williams-dev <contact@shanewilliams.info>
2014-09-09 12:10:23 +1000

    Added User Interface

--- src/first_tier/UserInterface.java
+++ src/first_tier/UserInterface.java
@@ -1,186 +1,177 @@
 package first_tier;
 
-import java.util.*; // for Scanner and ArrayList
 import java.io.*; // for FileNotFoundException
 
-import second_tier.*;
-import second_tier.PurchaseReminder;
-import second_tier.UniReminder;
+import javax.swing.JOptionPane;
 
+import second_tier.*;
+/**
+* Scenario Stage 2 demo program - 1st-tier class.
+
+**/
 public class UserInterface {
 	public void begin () {
-		Scanner input = new Scanner (System.in);
-		// Creates the Reminder ArrayList object
 		DataStorage reminder = new DataStorage();
 		// Display menu and Process Uni and Purchase Reminders
-		boolean finished = false, openedFile = false, fileSaved = true;
-		String fileName = "data.obj"; // Assumes that the file is named data.obj
+		boolean finished = false, fileSaved = true;
+		String objFile = "data.obj"; // Assumes that the file is named data.obj
+		
+		try {
+			ReminderWorker rw = new ReminderWorker ();
+			reminder.addAll (rw.openFile (objFile));
+			JOptionPane.showMessageDialog (null, "\n** File successfully opened **\n", "Success",  JOptionPane.PLAIN_MESSAGE);
+		}
+		catch (FileNotFoundException fnfe) {
+			JOptionPane.showMessageDialog (null, "\n**** ERROR: Cannot find the data file ****\n", "ERROR",  JOptionPane.ERROR_MESSAGE);
+		}
+		catch (IOException io) {
+			JOptionPane.showMessageDialog (null, "\n*** ERROR: Cannot perform I/O operation ***\n", "ERROR",  JOptionPane.ERROR_MESSAGE);
+			finished = true; // stops the while loop - ERROR stop
+		}
+		catch (ClassNotFoundException cnfe) {
+			JOptionPane.showMessageDialog (null, "\n** ERROR: Cannot read file correctly **\n", "ERROR",  JOptionPane.ERROR_MESSAGE);
+			finished = true; // stops the while loop - ERROR stop
+		}
+		if (finished) {
+			JOptionPane.showMessageDialog (null, "\n*** Fatal ERROR - Program Ended ***\n" +
+					"\n*** Please contact you computer services centre ***\n", "ERROR",  JOptionPane.ERROR_MESSAGE);
+		}
+		
 		while (!finished) {
-			int selection = showMenu (input);
+			int selection = showMenu ();
 			switch (selection) {
-				case 1: try {
-								if (!openedFile) { // addAll() Appends the file data to the ArrayList data - Comes from ReminderWorker in 2nd tier.
-									ReminderWorker pw = new ReminderWorker ();
-									reminder.addAll (pw.openFile (fileName));
-									System.out.println ("\n** File successfully opened **\n");
-									openedFile = true;
-								} else { // Don't double-up the data in the ArrayList
-									System.out.println ("\n*** ERROR: File was not found or has already been opened ***\n");
-								}
-							}
-							catch (ClassNotFoundException cnfe) {
-								System.out.println ("\n**** ERROR: Problem with data type ****");
-								openedFile = true;
-							}
-							catch (FileNotFoundException fnfe) {
-								System.out.println ("\n**** ERROR: Cannot find the data file ****\n");
-								openedFile = true; // To prevent doubling-up the data in the ArrayList
-							}
-							catch (IOException io) {
-								System.out.println ("\n*** ERROR: Cannot perform I/O operation ***\n");
-							}
-							break;
-				case 2: reminder.add (addPurchaseReminder (input));
+				case 1: reminder.add (addPurchaseReminder ());
 							fileSaved = false;
 							break;
-				case 3: reminder.add (addUniReminder (input));
+				case 2: reminder.add (addUniReminder ());
 							fileSaved = false;
 							break;
-				case 4: System.out.println ("\nDisplaying the Reminder Information information ...");
+				case 3: JOptionPane.showMessageDialog (null, "Displaying the Reminder information ...", "Reminder List", JOptionPane.PLAIN_MESSAGE);
 							for (int i = 0; i < reminder.size(); i++) {
 								// When displaying the contents of an object using print() or println()
 								// Java automatically looks for a toString() method, and, if available
 								// uses it to display the object contents.
 								// Polymorphism ensures that the correct data is displayed for each
-								// Uni or Purchase Reminder.
-								System.out.println (reminder.get(i)); // equivalent to p.get(i).toString()
-								System.out.println ("---------------------------------");
+								// Employee or Student.
+								JOptionPane.showMessageDialog (null, reminder.get(i), "Reminder Details", JOptionPane.PLAIN_MESSAGE);
 							}
 							break;
-				case 5: try {
-								if (!openedFile) {
-									System.out.println ("WARNING: This action will overwrite the data in the file -->");
-									System.out.println ("Please open the file before saving!!");
-								} else {
-									fileSaved = saveFile (fileName, reminder);
-								}
+				case 4: try {
+								fileSaved = saveFile (objFile, reminder);
 							}
-				  			catch (IOException ioe) {
-				  				System.out.println ("\n**** ERROR: Data cannot be saved ****\n");
-				  			}
+							catch (IOException ioe) {
+								JOptionPane.showMessageDialog (null, "**** ERROR: Data cannot be saved ****\n" +
+										"*** Fatal ERROR Program Ended ***\n" +
+										"*** Please contact you computer services centre ***",
+										"FATAL ERROR", JOptionPane.ERROR_MESSAGE);
+								finished = true; // stops the while loop - ERROR stop
+							}
 				  			break;
-				case 6: if (!fileSaved) {
-								String save = null;
-								System.out.println ("\n*** WARNING: New data has NOT been saved ***\n");
-								do { // Loop until user inputs y or n
-									System.out.print ("Do you wish to save the new data? (y or n) ");
-									save = input.nextLine().trim().toLowerCase();
-								} while (!save.equals("n") && !save.equals("y"));
-								if (save.equals("y")) {
-									try {
-										saveFile (fileName, reminder);
-									}
-						  			catch (IOException ioe) {
-						  				System.out.println ("\n**** ERROR: Data cannot be saved ****\n");
-						  			}
-						  		}
-							}
-							finished = true; // Stops the while loop
-							System.out.println ("\n*** Program Ended ***");
-							System.out.println ("*** Thank you for using the Reminder App ***\n");
-							break;
-				default: System.out.println ("\n** Invalid Selection **\n");
+				case 5: String fatalStop = "OK";
+							if (!fileSaved) {
+							String save = "y";
+							JOptionPane.showMessageDialog (null, "*** WARNING: New data has NOT been saved ***", "WARNING", JOptionPane.WARNING_MESSAGE);
+							do { // loop until we get y or n
+								save = JOptionPane.showInputDialog ("Do you wish to save the new data? (y or n) ").trim().toLowerCase();
+							} while (!save.equals("n") && !save.equals("y"));
+							if (save.equals("y")) {
+								try {
+									saveFile (objFile, reminder);
+								}
+					  			catch (IOException ioe) {
+					  				JOptionPane.showMessageDialog (null, "**** ERROR: Data cannot be saved ****\n" +
+					  						"*** Fatal ERROR Program Ended ***\n" +
+					  						"*** Please contact you computer services centre ***",
+					  						"FATAL ERROR", JOptionPane.ERROR_MESSAGE);
+					  				fatalStop = "ERROR";
+					  			}
+					  		}
+						}
+						finished = true; // stops the while loop - normal & ERROR
+						if (fatalStop.equals("OK")) {
+							JOptionPane.showMessageDialog (null, "*** Program Ended ***\n" +
+									"*** Thank you for using this program ***");
+						}
+						break;
+				default: JOptionPane.showMessageDialog (null, "\n** Invalid Selection **\n", "ERROR", JOptionPane.ERROR_MESSAGE);
 			} // end switch
 		} // end while loop
 	} // end begin()
 
-	public int showMenu (Scanner input) {
-		int selection = 0, count = 0;
-		System.out.println ("\n******* MENU *******\n");
-		System.out.println (++count + ". Open Existing File");
-		System.out.println (++count + ". Add a new Purchase Reminder");
-		System.out.println (++count + ". Add a new Uni Reminder");
-		System.out.println (++count + ". Display all details");
-		System.out.println (++count + ". Save All Data to File");
-		System.out.println (++count + ". Exit Program\n");
-		System.out.print ("Type the number of your selection, and tap the Enter key: ");
-		selection = input.nextInt();
-		input.nextLine(); // flush the input line
+	public int showMenu () {
+		int selection = 0;
+		String stringSelection = JOptionPane.showInputDialog (
+				"******* MENU *******\n\n" +
+				"1. Add a new Purchase Reminder\n" +
+				"2. Add a new Uni Reminder\n" +
+				"3. Display all Details\n" +
+				"4. Save All Data to File\n" +
+				"5. Exit Program\n\n" +
+				"Type the number of your selection, and click OK: ");
+		selection = Integer.parseInt (stringSelection.trim());
 		return selection;
-	} // End of showMenu()
+	} // end showMenu()
 
-	public UniReminder addUniReminder (Scanner input) {
+	public UniReminder addUniReminder () {
+		UniReminder u = new UniReminder ();
+		
 		//Reminder Name
-		System.out.print ("\nReminder Name: ");
-		String UrN = input.nextLine().trim(); 	
+		String UrN = (JOptionPane.showInputDialog (null, "\nWhat is the reminders name? ")).trim();  // trim() gets rid of leading & trailing whitespace
+		u.setTitle (UrN);	
 		
 		// Description
-		System.out.print (UrN+" description: ");
-		String UrD = input.nextLine().trim();
+		String UrD = (JOptionPane.showInputDialog (null, "Describe " + UrN + ": ")).trim();  // trim() gets rid of leading & trailing whitespace
+		u.setMessage (UrD);
 		
 		// Time
-		System.out.print (UrN+" at time (seperate hours and minutes with a full stop): ");
-		String StringUtime = input.nextLine().trim();
-		double Utime = Double.parseDouble(StringUtime);
+		String StringUTime = (JOptionPane.showInputDialog (null, "Time of " + UrN + ": ")).trim();  // trim() gets rid of leading & trailing whitespace
+		double UTime = Double.parseDouble(StringUTime);
+		u.setTime (UTime);
 		
 		// Subject Name
-		System.out.print (UrN+" subject Name: ");
-		String UrS = input.nextLine().trim();
+		String UrS = (JOptionPane.showInputDialog (null, "What subject is" + UrN + "is for? ")).trim();  // trim() gets rid of leading & trailing whitespace
+		u.setSubject (UrS);
 		
 		// Group Member Names
-		System.out.print (UrN+" group member names (if any): ");
-		String UgM = input.nextLine().trim();
+		String UgM = (JOptionPane.showInputDialog (null, "Who is in your group?")).trim();  // trim() gets rid of leading & trailing whitespace
+		u.setGroupMembers (UgM);
 		
 		// Urgency
-		System.out.print (UrN+" urgency (out of 10): ");
-		int Uu = input.nextInt();
-		
-		input.nextLine();
-		
-		// Uni Reminder object created based on inputs and returned out of the function
-		UniReminder u = new UniReminder (UrN, UrD, (double) Utime, UrS, UgM, (int) Uu);
-		
-		// *** Added success message
-		System.out.println("*** University Reminder input successfully ***");
+		String UuInt = (JOptionPane.showInputDialog (null, "How urgent is " + UrN + "? ")).trim();  // trim() gets rid of leading & trailing whitespace
+		int Uu = Integer.parseInt(UuInt);
+		u.setUrgency (Uu);
 		
 		return u;
 	}	// end addUniReminder()
 
-	public PurchaseReminder addPurchaseReminder (Scanner input) {
+	public PurchaseReminder addPurchaseReminder () {
+		PurchaseReminder p = new PurchaseReminder ();
 
-
-		// Reminder Name
-		System.out.print ("\nReminder Name: ");
-		String UrN = input.nextLine().trim(); 			
+		//Reminder Name
+		String PrN = (JOptionPane.showInputDialog (null, "\nWhat is the reminders name? ")).trim();  // trim() gets rid of leading & trailing whitespace
+		p.setTitle (PrN);	
+				
+		// Description
+		String PrD = (JOptionPane.showInputDialog (null, "Describe " + PrN + ": ")).trim();  // trim() gets rid of leading & trailing whitespace
+		p.setMessage (PrD);
+				
+		// Time
+		String StringPTime = (JOptionPane.showInputDialog (null, "Time of (use a . to seperate minutes and hours)" + PrN + ": ")).trim();  // trim() gets rid of leading & trailing whitespace
+		double PTime = Double.parseDouble(StringPTime);
+		p.setTime (PTime);
 		
 		// Description
-		System.out.print (UrN+" description: ");
-		String UrD = input.nextLine().trim();
+		String PrI = (JOptionPane.showInputDialog (null, "What is the name of the item? ")).trim();  // trim() gets rid of leading & trailing whitespace
+		p.setItem (PrI);
 		
-		// Time
-		System.out.print (UrN+" at time (seperate hours and minutes with a full stop): ");
-		String StringUtime = input.nextLine().trim();
-		double Utime = Double.parseDouble(StringUtime);
+		// Description
+		String PiD = (JOptionPane.showInputDialog (null, "Describe the Item or give its brand name: ")).trim();  // trim() gets rid of leading & trailing whitespace
+		p.setItemDescription (PiD);
 		
-		// Item Name
-		System.out.print (UrN+" brand name: ");
-		String UiN = input.nextLine().trim();
-		
-		// Item Description
-		System.out.print (UrN+" extra item information (if any): ");
-		String UdN = input.nextLine().trim();
-		
-		// Estimated Price
-		System.out.print (UrN+" estimated price (seperate dollars and cents with a full stop): $");
-		double UeP = input.nextDouble();
-
-		input.nextLine();
-		
-		// Purchase Reminder object creation based on inputs and returned out of the function
-		PurchaseReminder p = new PurchaseReminder (UrN, UrD, Utime, UiN, UdN, (double) UeP);
-		
-		// *** Added success message
-		System.out.println("\n*** Purchase Reminder input successfully ***");
+		// Price
+		String StringPPrice = (JOptionPane.showInputDialog (null, "Estimate how much " + PrI + " will cost: ")).trim();  // trim() gets rid of leading & trailing whitespace
+		double PPrice = Double.parseDouble(StringPPrice);
+		p.setTime (PPrice);
 		
 		return p;
 
@@ -188,9 +179,9 @@ public class UserInterface {
 
 
 	public boolean saveFile (String fileName, DataStorage list) throws IOException {
-		ReminderWorker pw = new ReminderWorker ();
-		pw.saveFile (fileName, list); // assuming file name
-		System.out.println ("\n** Data Successfully Saved **\n");
+		ReminderWorker rw = new ReminderWorker ();
+		rw.saveFile (fileName, list); // assuming file name
+		JOptionPane.showMessageDialog (null, "\n** Data Successfully Saved **\n");
 		return true;
 	}
 
